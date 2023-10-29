67870010f4440d9b0645d18114a6cdb7199ffe88
savagegraphics <olawaleraji188@gmail.com>
2023-10-29 05:12:12 -0700

    first commit

---
+++ app/dashboard/sidenav.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+type Props = {}
+
+const sidenav = (props: Props) => {
+  return <div>sidenav</div>
+}
+
+export default sidenav
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,20 @@
-export default function RootLayout({
-  children,
+import '@/app/ui/global.css'
+import { Inter, Lusitana } from 'next/font/google'
+
+export const inter = Inter({ subsets: ['latin'] })
+
+export const lusitana = Lusitana({
+  weight: ['400', '700'],
+  subsets: ['latin']
+})
+export default function RootLayout ({
+  children
 }: {
-  children: React.ReactNode;
+  children: React.ReactNode
 }) {
   return (
-    <html lang="en">
-      <body>{children}</body>
+    <html lang='en'>
+      <body className={`${inter.className} antialiased`}>{children}</body>
     </html>
-  );
+  )
 }
